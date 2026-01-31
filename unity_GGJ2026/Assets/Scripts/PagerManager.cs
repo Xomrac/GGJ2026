b6f689a62692d062d9e574903e2543c99c9ea3e6
Xomrac <morettimarco2759@gmail.com>
2026-01-31 08:47:25 +0100

    basic gameplay look

--- unity_GGJ2026/Assets/Scripts/PagerManager.cs
+++ unity_GGJ2026/Assets/Scripts/PagerManager.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using DefaultNamespace;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
@@ -7,6 +9,15 @@ public class PagerManager : MonoBehaviour
 {
 	[SerializeField] private TextMeshProUGUI _pagerText;
 	[SerializeField] private TextMeshProUGUI _timeText;
+	[SerializeField] private RectTransform _pagerTransform;
+	[SerializeField] private float _timeBeforeHide = 2f;
+	[SerializeField] private float _animationsDuration = 0.5f;
+
+	
+
+	
+
+	
 	
 	private DeliveriesManager _deliveriesManager;
 	
@@ -20,16 +31,29 @@ public class PagerManager : MonoBehaviour
 		_deliveriesManager.DeliveryCompleted += OnDeliveryCompleted;
 	}
 
+	private void Start()
+	{
+		_pagerTransform.anchoredPosition = new Vector2(0, 0);
+	}
+
 	private void OnDeliveryCompleted(DeliveryData _)
 	{
-		_pagerText.text = "Delivery Completed!";
+		_pagerText.text = "OK!";
 		_timeText.text = "--:--";
+		StartCoroutine(WaitAndSetPosY(0));
 	}
 
 	private void OnDeliveryFailed(DeliveryData _)
 	{
-		_pagerText.text = "Delivery Failed! - FAI CACARE";
+		_pagerText.text = "KO!";
 		_timeText.text = "--:--";
+		StartCoroutine(WaitAndSetPosY(0));
+	}
+
+	private IEnumerator WaitAndSetPosY(float posY)
+	{
+		yield return new WaitForSeconds(_timeBeforeHide);
+		_pagerTransform.DOAnchorPosY(posY, _animationsDuration).SetEase(Ease.InOutBack);
 	}
 
 	private void OnDeliveryTimeUpdated(float remainingTime)
@@ -39,6 +63,8 @@ public class PagerManager : MonoBehaviour
 
 	private void OnNewDeliveryCreated(DeliveryData deliveryData)
 	{
-		_pagerText.text = $"{deliveryData.ToDeliver.name} to {deliveryData.DeliveryLocation.DeskName}";
+		_pagerText.text = $"{deliveryData.ToDeliver.PagerCode} - {deliveryData.DeliveryLocation.PagerCode}";
+		_timeText.text = TimeSpan.FromSeconds(deliveryData.DeliveryTime).ToString(@"mm\:ss");
+		_pagerTransform.DOAnchorPosY(_pagerTransform.sizeDelta.y, _animationsDuration).SetEase(Ease.InOutBack);
 	}
 }
