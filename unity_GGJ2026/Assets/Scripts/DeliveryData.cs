8332106278b0267a8c3fd06062a4e359dcc78e40
Xomrac <morettimarco2759@gmail.com>
2026-01-31 12:35:12 +0100

    updated pick up and drop feature

--- unity_GGJ2026/Assets/Scripts/DeliveryData.cs
+++ unity_GGJ2026/Assets/Scripts/DeliveryData.cs
@@ -6,17 +6,21 @@ public class DeliveryData
 	private DeliverableData _toDeliver;
 	public DeliverableData ToDeliver => _toDeliver;
 	
-	private Desk _deliveryLocation;
-	public Desk DeliveryLocation => _deliveryLocation;
+	private DropoffZone _deliveryLocation;
+	public DropoffZone DeliveryLocation => _deliveryLocation;
 	
 	private float _deliveryTime;
 	public float DeliveryTime => _deliveryTime;
+	
+	public float elapsedTime;
+	public float score;
 
-	public DeliveryData(DeliverableData go, Desk delivery, float deliveryTime)
+	public DeliveryData(DeliverableData go, DropoffZone delivery, float deliveryTime)
 	{
 		_toDeliver = go;
 		_deliveryLocation = delivery;
 		_deliveryTime = deliveryTime;
+		elapsedTime = 0f;
 	}
 	
 }
